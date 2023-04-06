bc125ff21278323ea9569eb2722018f0a9533e53
Courtney <cjpike@bu.edu>
2023-04-06 18:44:09 -0400

    proxy

--- src/api.js
+++ src/api.js
@@ -34,7 +34,7 @@ const router = express.Router();
 
 app.use(cors())
 app.use(cookieParser())
-app.use(`/.netlify/functions/api`, (proxy()))
+app.use(proxy())
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
 app.use(`/.netlify/functions/api`, router)
