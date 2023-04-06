57808148aba508e8a30f87a2f8f6c38ee4c716ae
Courtney <cjpike@bu.edu>
2023-04-06 19:41:33 -0400

    change wording for obscurity

--- dist/handleJSON.js
+++ dist/handleJSON.js
@@ -22,11 +22,11 @@ function findPopularityTracksJSON(data){
   overallTotalPopularity = overallTotalPopularity/data.length
   div.innerHTML = 
   `<div class ="row"><h3 id ="colHead">
-  <span style="color: black">Obscurity Ranking</span><br>
+  <span style="color: black">How Obscure is Your Music Taste?</span><br>
   <span style="font-size: 0.7rem"> <span style="font-family: 'Monoton', sans-serif;"> 
-  Your top tracks rank </span> <span style="color: black; font-size: 0.9rem">` 
+  </span> <span style="color: black; font-size: 0.9rem">` 
   + Math.trunc(userTotalPopularity) + '</span> out of <span style="color: black; font-size: 0.9rem">' + overallTotalPopularity + 
-  `</span> on Spotify's popularity index</h3></div>`
+  `</span></h3></div>`
   mainContainer.appendChild(div);
 }
 
@@ -46,11 +46,11 @@ function findPopularityArtistsJSON(data){
   overallTotalPopularity = overallTotalPopularity/data.length
   div.innerHTML = 
   `<div class ="row"><h3 id ="colHead">
-  <span style="color: black">Obscurity Ranking</span><br>
-  <span style="font-size: 0.7rem"> <span style="font-family: 'Monoton', sans-serif;">
-  Your top artists rank </span><span style="color: black; font-size: 0.9rem">` 
+  <span style="color: black">How Obscure is Your Music Taste?</span><br>
+  <span style="font-size: 0.7rem"> <span style="font-family: 'Monoton', sans-serif;"> 
+  </span> <span style="color: black; font-size: 0.9rem">` 
   + Math.trunc(userTotalPopularity) + '</span> out of <span style="color: black; font-size: 0.9rem">' + overallTotalPopularity + 
-  `</span> on Spotify's popularity index</span></h3></div>`
+  `</span> </span></h3></div>`
   mainContainer.appendChild(div);
 }
 
